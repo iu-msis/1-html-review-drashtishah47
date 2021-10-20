74f1eb0254d8029341a6030ca2444e456d21d72b
drashtishah47 <dtshah47@gmail.com>
2021-10-20 11:45:21 -0400

    Add book

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -14,7 +14,7 @@
 //       /*pretty() {
 //         return dayjs(this.person.dob.date)
 //           .format('D MMM YYYY');
-  
+
 //       }
 //       */
 //     },
@@ -52,70 +52,85 @@
 // Vue.createApp(Person).mount('#personApp');
 
 const Offer = {
-  data() {
-    return {
-          "students": [],
-          "offers": [],
-          "books" :[],
-          "selectedStudent": null
-      }
-  },
-  computed: {
-      // prettyBirthday() {
-      //     return dayjs(this.person.dob.date)
-      //     .format('D MMM YYYY');
-      // }
-  },
-  methods: {
-      /*selectStudent(s) {
-          console.log("Clicked", s);
-          if (this.selectedStudent == s) {
-              return;
-          }
+    data() {
+        
+            return {
+                books: [],
+                bookForm: {},
+                selectedBook: null
+            }
+        },
+        computed: {
+            // prettyBirthday() {
+            //     return dayjs(this.person.dob.date)
+            //     .format('D MMM YYYY');
+            // }
+        },
+        methods: {
+            /*selectStudent(s) {
+                console.log("Clicked", s);
+                if (this.selectedStudent == s) {
+                    return;
+                }
+      
+                this.selectedStudent = s;
+                this.offers = [];
+                this.fetchOfferData(s);
+            },
+           /* fetchStudentData() {
+                fetch('/api/student/')
+                .then(response => response.json())
+                .then((parsedJson) => {
+                    console.log(parsedJson);
+                    this.students = parsedJson
+                })
+                .catch( err => {
+                    console.error(err)
+                })
+            },*/
+            fetchBooksData() {
+                fetch('/api/books/')
+                    .then(response => response.json())
+                    .then((parsedJson) => {
+                        console.log(parsedJson);
+                        this.books = parsedJson
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    })
+            },
 
-          this.selectedStudent = s;
-          this.offers = [];
-          this.fetchOfferData(s);
-      },
-     /* fetchStudentData() {
-          fetch('/api/student/')
-          .then(response => response.json())
-          .then((parsedJson) => {
-              console.log(parsedJson);
-              this.students = parsedJson
-          })
-          .catch( err => {
-              console.error(err)
-          })
-      },*/
-      fetchBooksData() {
-          fetch('/api/books/')
-          .then(response => response.json())
-          .then((parsedJson) => {
-              console.log(parsedJson);
-              this.books = parsedJson
-          })
-          .catch( err => {
-              console.error(err)
-          })
-      },
-      /*fetchOfferData(s) {
-          console.log("Fetching offers for", s);
-          fetch('/api/offer/?student=' + s.id)
-          .then(response => response.json())
-          .then((parsedJson) => {
-              console.log(parsedJson);
-              this.offers = parsedJson
-          })
-          .catch( err => {
-              console.error(err)
-          })
-      }*/
-  },
-  created() {
-      this.fetchBooksData();
-  }
-}
+            postNewBook(evt) {
+                //this.bookForm.bookId = this.selectedBook.id;        
+               // console.log("Posting:", this.bookForm);
+                // alert("Posting!");
+        
+                fetch('api/books/create.php', {
+                    method:'POST',
+                    body: JSON.stringify(this.bookForm),
+                    headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                    }
+                  })
+                .then( response => response.json() )
+                .then( json => {
+                    console.log("Returned from post:", json);
+                    // TODO: test a result was returned!
+                    this.books = json;
+                    
+                    // reset the form
+                    this.bookForm = {};
+                  });
+            }, 
+            // handleResetBook(){
+            //     this.selectedBook = null;
+            //     this.bookForm = {};
+            // }   
+        },
+        created() {
+            this.fetchBooksData();
+        }
+    }
 
 Vue.createApp(Offer).mount('#offerApp');
 
